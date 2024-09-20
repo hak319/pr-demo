845438f84cacb526728b618725566962fcd1084c
Hak319 <134386603+hak319@users.noreply.github.com>
2024-09-20 13:04:01 +0900

    Add files via upload

---
+++ pages/duduge.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react"
+
+export default function Duduge() {
+    useEffect(()=> {
+        window.location.href = 'duduge/index.html'
+    })
+    return null;
+  }
+  
---
+++ pages/snake.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react"
+
+export default function Duduge() {
+    useEffect(()=> {
+        window.location.href = 'snake/index.html'
+    })
+    return null;
+  }
+  
